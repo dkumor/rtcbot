162f3f0a9874c9ff2e25982424394066ad63a5f5
Daniel R. Kumor <daniel@dkumor.com>
2019-03-10 18:18:22 -0400

    Added setup.py, getting ready for pypi

---
+++ setup.py
@@ -0,0 +1,32 @@
+import pathlib
+import setuptools
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setuptools.setup(
+    name="rtcbot",
+    version="0.0.1",
+    description="An asyncio-focused library for webrtc robot control",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dkumor/rtcbot",
+    author="Daniel Kumor",
+    author_email="daniel@dkumor.com",
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    python_requires=">=3.7.0",
+    install_requires=["aiortc" "pyserial", "pyserial-asyncio", "numpy", "aiohttp"],
+)
